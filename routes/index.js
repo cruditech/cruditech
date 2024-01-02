c9bc5859f198a0c764d457a008f40cd97237375d
Erin A <erinakarice@gmail.com>
2024-01-02 07:21:41 +0000

    Beautiful🤩

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,34 @@ var router = require('express').Router();
 const { requiresAuth } = require('express-openid-connect');
 
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Auth0 Webapp sample Nodejs',
+  res.render('portfolio-masonry', {
+    title: 'Cruditech',
     isAuthenticated: req.oidc.isAuthenticated()
   });
+});
+
+router.get('/disclosure', function (req, res, next) {
+  res.render('disclosure');
+});
+
+router.get('/eula', function (req, res, next) {
+  res.render('eula');
+});
+
+router.get('/privacy', function (req, res, next) {
+  res.render('privacy-policy');
+});
+
+router.get('/returns', function (req, res, next) {
+  res.render('return-policy');
+});
+
+router.get('/terms', function (req, res, next) {
+  res.render('terms-of-use');
+});
+
+router.get('/use', function (req, res, next) {
+  res.render('acceptable-use');
 });
 
 router.get('/profile', requiresAuth(), function (req, res, next) {
--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 const dotenv = require('dotenv');
 const express = require('express');
+const { engine }  = require('express-handlebars');
 const http = require('http');
 const logger = require('morgan');
 const path = require('path');
 const router = require('./routes/index');
 const { auth } = require('express-openid-connect');
-
-dotenv.load();
-
+dotenv.config();
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpackConfig = require('./webpack.config.js');
 const app = express();
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
+const compiler = webpack(webpackConfig);
+app.set('views', path.join(__dirname, 'src/pews/'));
+app.engine('handlebars', engine({
+  defaultLayout: false,
+  // partialsDir: path.join(__dirname, 'src/'),
+}));
+app.set('view engine', 'handlebars');
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: webpackConfig.output.publicPath
+}));
+app.use(webpackHotMiddleware(compiler));
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-
 const config = {
   authRequired: false,
   auth0Logout: true
@@ -25,6 +34,8 @@ const config = {
 const port = process.env.PORT || 3000;
 if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
   config.baseURL = `http://localhost:${port}`;
+} else {
+  config.baseURL = process.env.BASE_URL;
 }
 
 app.use(auth(config));
@@ -36,6 +47,9 @@ app.use(function (req, res, next) {
 });
 
 app.use('/', router);
+// app.get('/', function (req, res) {
+//   res.render('privacy-policy');
+// });
 
 // Catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -47,13 +61,15 @@ app.use(function (req, res, next) {
 // Error handlers
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: process.env.NODE_ENV !== 'production' ? err : {}
-  });
+  // res.render('error', {
+  //   message: err.message,
+  //   error: process.env.NODE_ENV !== 'production' ? err : {}
+  // });
+  res.send(err.message);
 });
 
 http.createServer(app)
   .listen(port, () => {
     console.log(`Listening on ${config.baseURL}`);
   });
+
